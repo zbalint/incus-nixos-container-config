3526911e7abde08ac9c8933e19decde00429477b
zbalint <balint.zoltan.marton@gmail.com>
2025-08-27 00:48:59 +0200

    Update flake.nix with git package.

--- flake.nix
+++ flake.nix
@@ -16,24 +16,29 @@
 
           virtualisation.podman.enable = true;
           virtualisation.podman.dockerCompat = true;
+
+          # Install packages system-wide
+          environment.systemPackages = with nixpkgs.legacyPackages.x86_64-linux; [
+            git
+          ];
         }
         {
           # Add a new user "styx"
           users.users.styx = {
-            isNormalUser = true; # marks as a normal user
-            description = "styx"; # optional
-            hashedPassword = "$y$j9T$0d6Gx30pnve.klhKgmZwe0$JrdRjFcObsc59RL/ug5fu0DQDdCwKnSOb.b8KIYw2G5"; # password
-            uid = 2000; # set user id
-            extraGroups = [ "wheel" ]; # sudo access
+            isNormalUser = true;
+            description = "styx";
+            hashedPassword = "$y$j9T$0d6Gx30pnve.klhKgmZwe0$JrdRjFcObsc59RL/ug5fu0DQDdCwKnSOb.b8KIYw2G5";
+            uid = 2000;
+            extraGroups = [ "wheel" ];
           };
           # Add a new user "tartarus"
           users.users.tartarus = {
-            isNormalUser = true; # marks as a normal user
-            description = "tartarus"; # optional
-            hashedPassword = "$y$j9T$zPyT0FHrHcnf93tR6oupe/$XP2h7uePCAF8Z6U6xb9eTFDIf5Va9G1tVrOnuwSmIb0"; # password
+            isNormalUser = true;
+            description = "tartarus";
+            hashedPassword = "$y$j9T$zPyT0FHrHcnf93tR6oupe/$XP2h7uePCAF8Z6U6xb9eTFDIf5Va9G1tVrOnuwSmIb0";
             linger = true;
-            uid = 4000; # set user id
-            extraGroups = [ "podman" ]; # podman access
+            uid = 4000;
+            extraGroups = [ "podman" ];
           };
         }
       ];
