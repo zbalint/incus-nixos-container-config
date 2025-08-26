0ba4e598c8c90672e6c09ad15f4d636e3d40f840
zbalint <balint.zoltan.marton@gmail.com>
2025-08-27 01:45:19 +0200

    wip

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     "${modulesPath}/virtualisation/lxc-container.nix"
-    ./incus.nix
+    (builtins.path { path = "/etc/nixos/incus.nix"; })
   ];
 
   networking = {
--- flake.nix
+++ flake.nix
@@ -48,5 +48,4 @@
       ];
     };
   };
-  extraFiles = [ "incus.nix" ];
 }
